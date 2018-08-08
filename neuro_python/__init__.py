361004c2a77df1d0e1459260d0bcd8f0e272d99f
SnowdenLeeBates <lee.bates@snowdengroup.com>
2018-08-08 09:59:53 +0800

    update

--- neuro_python/__init__.py
+++ neuro_python/__init__.py
@@ -3,11 +3,11 @@ The neuro_python package provides access to Neuroverse from within Python.
 
 It currently contains:
     - Neuro_Call class: Gives authorised access to the Neuroverse API
-    - neuro_data module: Gives a user of python access to Neuroverse Data Stores
+    - neuro_data module(nd): Gives a user of python access to Neuroverse Data Stores
 """
 
 def home_directory():
     return "/home/jovyan/session"
 
-from . import neuro_data
+from . import neuro_data as nd
 from . neuro_call import neuro_call
