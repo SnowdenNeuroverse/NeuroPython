7a0ebecd8efe18202e3fda64e67ef9aa2c820b9a
SnowdenLeeBates <lee.bates@snowdengroup.com>
2018-08-08 09:48:59 +0800

    update

--- neuro_python/neuro_data/__init__.py
+++ neuro_python/neuro_data/__init__.py
@@ -1,4 +1,9 @@
-
+"""
+The neuro_data module contains:
+    schema_manager(sm): Creates and stores the schema info for Neuroverse tables
+    stream_table(st): Stream data from a source to a sink table in Neuroverse
+    source_sink(ss): Source and sink parameters
+"""
 from neuro_python.neuro_data import schema_manager as sm
-from neuro_python.neuro_data import stream_tables as st
+from neuro_python.neuro_data import stream_table as st
 from neuro_python.neuro_data import source_sink as ss
