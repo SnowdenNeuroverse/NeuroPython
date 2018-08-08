83f2658162781c10b5fea6cdcedfcc3588a0e549
SnowdenLeeBates <lee.bates@snowdengroup.com>
2018-08-08 13:21:12 +0800

    update

--- neuro_python/neuro_data/__init__.py
+++ neuro_python/neuro_data/__init__.py
@@ -7,3 +7,4 @@ The neuro_data module contains:
 from neuro_python.neuro_data import schema_manager as sm
 from neuro_python.neuro_data import stream_table as st
 from neuro_python.neuro_data import source_sink as ss
+from neuro_python.neuro_data import datalake_commands as dc
