e3a6f154bfab37aaf094410e60cf39a143053ddc
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2018-10-19 15:20:31 +0800

    Update spark_manager.py

--- neuro_python/neuro_compute/spark_manager.py
+++ neuro_python/neuro_compute/spark_manager.py
@@ -150,7 +150,7 @@ def load_pyspark_notebook_to_str(file_name: str):
     Read a python notebook as a string into a variable. This variable can be given to submit_job
     """
     tmp_file=str(uuid.uuid4())
-    !jupyter nbconvert --to script $file_name --output $file
+    os.system("jupyter nbconvert --to script " + file_name +" --output " + tmp_file)
     file=open(tmp_file+".py")
     script=file.read()
     file.close()
