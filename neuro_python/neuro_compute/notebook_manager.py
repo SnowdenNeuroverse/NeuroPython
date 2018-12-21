edffb803de6950a106086304480e0e6fff4c45d6
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2018-12-21 14:22:42 +0800

    Update notebook_manager.py

--- neuro_python/neuro_compute/notebook_manager.py
+++ neuro_python/neuro_compute/notebook_manager.py
@@ -5,8 +5,8 @@ Notebook Manager in Neuroverse.
 
 from neuro_python.neuro_call import neuro_call
 
-def list_active_sessions(job_id: str):
+def list_active_sessions():
     """
     Remove a spark manager job
     """
-    neuro_call("80", "notebookmanagementservice", "GetDetailedSessionList", None)
+    neuro_call("8080", "notebookmanagementservice", "GetDetailedSessionList", None)
