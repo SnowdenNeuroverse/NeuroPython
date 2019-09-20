c772bc4d2a818e58be406c77c5635986c1e4de7f
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2019-09-20 12:59:37 +0800

    Update notebook_manager.py

--- neuro_python/neuro_compute/notebook_manager.py
+++ neuro_python/neuro_compute/notebook_manager.py
@@ -2,6 +2,7 @@
 The notebook_manager module provides functions to interact with the
 Notebook Manager in Neuroverse.
 """
+import datetime
 
 from neuro_python.neuro_call import neuro_call
 
@@ -10,3 +11,9 @@ def list_active_sessions():
     Remove a spark manager job
     """
     return neuro_call("8080", "notebookmanagementservice", "GetDetailedSessionList", None)
+
+last_session_delay_run=datetime.datetime(1970, 1, 1, 8, 0)
+def delay_session_shutdown():
+    if datetime.datetime.utcnow()-last_session_delay_run>datetime.timedelta(minutes=5):
+        last_session_delay_run=datetime.datetime.utcnow()
+        return neuro_call("8080", "notebookmanagementservice", "KeepSessionRunning", None)
