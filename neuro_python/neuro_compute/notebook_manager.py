8faed56f9e2a728586efb4b7d4bd4e358cac50c2
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2019-09-20 13:03:48 +0800

    Update notebook_manager.py

--- neuro_python/neuro_compute/notebook_manager.py
+++ neuro_python/neuro_compute/notebook_manager.py
@@ -8,12 +8,19 @@ from neuro_python.neuro_call import neuro_call
 
 def list_active_sessions():
     """
-    Remove a spark manager job
+    Get list of active notebook sessions
     """
     return neuro_call("8080", "notebookmanagementservice", "GetDetailedSessionList", None)
 
 last_session_delay_run=datetime.datetime(1970, 1, 1, 8, 0)
+KeepSessionRunningResponse=''
 def delay_session_shutdown():
+    """
+    Delay a notebook session from being shutdown automatically
+    """
     if datetime.datetime.utcnow()-last_session_delay_run>datetime.timedelta(minutes=5):
         last_session_delay_run=datetime.datetime.utcnow()
-        return neuro_call("8080", "notebookmanagementservice", "KeepSessionRunning", None)
+        KeepSessionRunningResponse=neuro_call("8080", "notebookmanagementservice", "KeepSessionRunning", None)
+        return KeepSessionRunningResponse
+    else:
+        return KeepSessionRunningResponse
