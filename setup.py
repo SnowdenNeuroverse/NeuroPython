f0972c4468a1f100596a00428e5f42a96b9285ca
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2019-09-25 11:09:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 import subprocess
 subprocess.check_call(["python", '-m', 'pip', 'install', 'SQLAlchemy==1.3.3']) # install pkg
+subprocess.check_call(["python", '-m', 'pip', 'install', 'retrying==1.3.3']) # install pkg
 
 setup(name='neuro_python',
       packages=find_packages())
