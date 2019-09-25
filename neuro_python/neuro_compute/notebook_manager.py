0837842fdb796ecf977ea71563bd67d918292d68
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2019-09-25 11:08:18 +0800

    Update notebook_manager.py

--- neuro_python/neuro_compute/notebook_manager.py
+++ neuro_python/neuro_compute/notebook_manager.py
@@ -3,6 +3,7 @@ The notebook_manager module provides functions to interact with the
 Notebook Manager in Neuroverse.
 """
 import datetime
+from retrying import retry
 
 from neuro_python.neuro_call import neuro_call
 
@@ -10,10 +11,12 @@ def list_active_sessions():
     """
     Get list of active notebook sessions
     """
+    
     return neuro_call("8080", "notebookmanagementservice", "GetDetailedSessionList", None)
 
 last_session_delay_run=datetime.datetime(1970, 1, 1, 8, 0)
 KeepSessionRunningResponse=''
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000,stop_max_attempt_number=7)
 def delay_session_shutdown():
     """
     Delay a notebook session from being shutdown automatically
