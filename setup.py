52736f7fc83fe333cd0eef91877c404db55fa00a
Phillip Pettit <Phillip.Pettit1@Downergroup.com>
2024-02-29 09:43:17 +0800

    dependency upgrade correction

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 import subprocess
 subprocess.check_call(["python", '-m', 'pip', 'install', 'SQLAlchemy==2.0.27']) # install pkg
-subprocess.check_call(["python", '-m', 'pip', 'install', 'scikit-learn==1.4.1']) # install pkg
+subprocess.check_call(["python", '-m', 'pip', 'install', 'scikit-learn==1.4.0']) # install pkg
 subprocess.check_call(["python", '-m', 'pip', 'install', 'retrying==1.3.4']) # install pkg
 
 setup(name='neuro_python',
